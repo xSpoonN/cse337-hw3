566772bc86b10ae4b81728c13f261958c66b6f1a
Kevin Tao <kevin.tao@stonybrook.edu>
2022-10-25 11:44:29 -0400

    Done?

--- treasure_hunt.rb
+++ treasure_hunt.rb
@@ -7,16 +7,153 @@ class Cave
                       [4, 3], [3, 12], [12, 9], [9, 19], [19, 20], [20, 17], [17, 7], [7, 6], [6, 5], [4, 14], [12, 13],
                       [18, 19], [16, 17], [15, 6], [14, 13], [13, 18], [18, 16], [16, 15], [15, 14]]
     # add cave attributes
+    @rooms = Hash.new
+    (1..20).each do |val|
+      @rooms[val] = Room.new(val)
+    end
+    @edges.each do |edge|
+      @rooms[edge[0]].connect(@rooms[edge[1]])
+    end
   end
   # add cave methods
+  def add_hazard(h, n)
+    noHazards = @rooms.reject {|key, value| value.has?(h)}
+    noHazards.values.sample(n).each do |rm|
+      rm.add(h)
+    end
+  end
+
+  def random_room()
+    return @rooms.values.sample()
+  end
+
+  def room_with(hazard)
+    @rooms.values.each do |rm|
+      if rm.has?(hazard) then return rm end
+    end
+    return nil
+  end
+
+  def move(hazard, frm, to)
+    frm.remove(hazard)
+    to.add(hazard)
+  end
+
+  def room(n)
+    if !@rooms.keys.include?(n) then return nil end
+    return @rooms[n]
+  end
+
+  def entrance() 
+    @rooms.values.each do |rm|
+      if rm.safe? then return rm end
+    end
+  end
 end
 
 class Player
   # add specified Player methods
+  attr_reader :room
+  def initialize()
+    @senses = Hash.new
+    @encounters = Hash.new
+    @actions = Hash.new
+    @room = 0
+  end
+
+  def sense(hazard, &callback)
+    @senses[hazard] = callback
+  end
+
+  def encounter(hazard, &callback)
+    @encounters[hazard] = callback
+  end
+
+  def action(act, &callback)
+    @actions[act] = callback
+  end
+
+  def enter(room)
+    @room = room
+    @encounters.keys.each do |hz|
+      if room.has?(hz) then return @encounters[hz].call() end
+    end
+  end
+
+  def explore_room
+    room.neighbors.each do |nb|
+      nb.hazards.each do |hz|
+        @senses[hz].call()
+      end
+    end
+  end
+
+  def act(action, destination) 
+    if !@actions.keys.include?(action) then raise KeyError end
+    @actions[action].call(destination)
+  end
+
 end
 
 class Room
+  attr_reader :number, :hazards, :neighbors
   # add specified Room methods
+  def initialize(number)
+    @number = number
+    @hazards = Array.new
+    @neighbors = Array.new
+  end
+
+  def add(hazard) 
+    @hazards.append(hazard)
+  end
+
+  def has?(hazard)
+    @hazards.include?(hazard)
+  end
+
+  def remove(hazard)
+    if !has?(hazard) then raise ValueError end
+    @hazards.delete(hazard)
+  end
+
+  def empty?()
+    @hazards.length == 0
+  end
+
+  def safe?()
+    if @hazards.length != 0 then return false end
+    @neighbors.each do |nb|
+      if nb.hazards.size != 0 then return false end
+    end
+    return true
+  end
+
+  def connect(other_room)
+    if @neighbors.include?(other_room) then return end
+    @neighbors.append(other_room)
+    other_room.connect(self)
+  end
+
+  def exits()
+    out = []
+    @neighbors.each do |nb|
+      out.append(nb.number)
+    end
+    return out
+  end
+  
+  def neighbor(number)
+    @neighbors.each do |nb|
+      if nb.number == number then return nb end
+    end
+    return nil
+  end
+
+  def random_neighbor()
+    if @neighbors.size == 0 then raise IndexError end
+    return @neighbors[rand(0...@neighbors.size)]
+  end
 end
 
 class Console
